0f5edc4938182bfe4fcee815fc27fd0846c731f4
Anitha chowdary <130220172+anitha-anii@users.noreply.github.com>
2023-06-18 23:32:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+
+let confirmed =confirm("Do you want to proceed?");
+
+if (confirmed) {
+  alert("You clicked OK. Proceeding...");
+ } else {
+ alert("You clicked Cancel. Exiting...");
+}
